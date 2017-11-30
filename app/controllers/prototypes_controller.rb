aeea8db1bd2a055576dc2f99a501d7d0112641fb
コミットしました <yumayo14@gmail.com>
2017-11-30 11:27:48 +0900

    Editアクション時に、投稿された画像データに上書きする処理

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -43,7 +43,7 @@ class PrototypesController < ApplicationController
       :catch_copy,
       :concept,
       :user_id,
-      captured_images_attributes: [:content, :status]
+      captured_images_attributes: [:id, :content, :status]
     )
   end
 end
