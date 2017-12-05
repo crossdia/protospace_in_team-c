358541fb90e4e8e0cfa9cc977018535c22058aa8
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-05 09:52:01 +0900

    Commentsコントローラーのcreateアクションとストロングパラメーターの定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,12 @@
 class CommentsController < ApplicationController
   def create
+  	@comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id],user_id: current_user.id)
+  	redirect_to "/prototypes/#{@comment.prototype.id}"
+  end
+
+  private
+  def comment_params
+  	params.permit(:text,:prototype_id)
   end
 
   def edit
