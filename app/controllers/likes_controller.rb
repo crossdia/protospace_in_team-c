45f4136278ee70689f722d3b8a9df7926c4e81f6
R-Msb <ryu@MBP.local>
2017-12-12 09:26:55 +0900

    fix controller
    リダイレクトを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,16 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.new(like_params.merge( {user_id: current_user.id}) )
-    if @like.save
-        redirect_to( {contoroller: :prototypes, action: :show}, prototype_id: params[:prototype_id])
+    like = Like.new(like_params.merge( {user_id: current_user.id}) )
+    if like.save
+        redirect_to( controller: :prototypes, action: :show, id: params[:prototype_id] )
     else
       render :root
     end
   end
 
   def delete
+
   end
 
   private
