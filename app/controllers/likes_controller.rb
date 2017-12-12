509be5470d7f583adb116c451106f4ec7791884d
R-Msb <ryu@MBP.local>
2017-12-12 09:20:55 +0900

    update likes_ctrl
    ボタンを押すとlikeを保存する処理を設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,19 @@
 class LikesController < ApplicationController
 
   def create
+    @like = Like.new(like_params.merge( {user_id: current_user.id}) )
+    if @like.save
+        redirect_to( {contoroller: :prototypes, action: :show}, prototype_id: params[:prototype_id])
+    else
+      render :root
+    end
   end
 
   def delete
+  end
+
+  private
+  def like_params
+    params.permit(:prototype_id)
   end
 end
